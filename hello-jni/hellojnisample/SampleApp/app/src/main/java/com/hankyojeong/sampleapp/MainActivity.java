b699806457ffb53df49a287c95dce48931199984
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-02-09 16:59:32 +0900

    refactor: Add shared library to Sample Android Application
    Making a Sample Android Application to using hello-jni.so

--- hello-jni/hellojnisample/SampleApp/app/src/main/java/com/hankyojeong/sampleapp/MainActivity.java
+++ hello-jni/hellojnisample/SampleApp/app/src/main/java/com/hankyojeong/sampleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.hankyojeong.sampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.hello_textview);
+
+        tv.setText(stringFromJNI());
+    }
+
+    /*
+    A native method that is implemented by the 'hello-jni' native library
+     */
+    public native String stringFromJNI();
+
+    /*
+    This is used to load the 'hello-jni' library on application startup
+     */
+    static {
+        System.loadLibrary("hello-jni");
     }
 }
